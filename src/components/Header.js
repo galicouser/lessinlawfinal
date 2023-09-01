4bb6f7300a2aa3fc7a3630d29bc85762d18d64ba
RajaAliHassan1 <60552815+RajaAliHassan1@users.noreply.github.com>
2023-09-01 21:55:45 +0500

    Update Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,26 @@
 import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import Grid from '@mui/material/Grid'
-import MenuIcon from '@mui/icons-material/Menu';
-import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
-import { motion } from "framer-motion";
+import MenuIcon from '@mui/icons-material/Menu'
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight'
+import { motion } from 'framer-motion'
 
 const Header = () => {
     const [currentMessageIndex, setCurrentMessageIndex] = useState(0)
 
+    const [navbar, setnavbar] = useState(false)
 
-    
-    const [navbar, setnavbar] = useState(false);
-  
     const handleScroll = () => {
-      const scrollY = window.scrollY;
-  
-      if (scrollY > 50) {
-        setnavbar(true);
-      } else {
-        setnavbar(false);
-      }
-    };
-    window.addEventListener("scroll", handleScroll);
-    
+        const scrollY = window.scrollY
+
+        if (scrollY > 50) {
+            setnavbar(true)
+        } else {
+            setnavbar(false)
+        }
+    }
+    window.addEventListener('scroll', handleScroll)
+
     useEffect(() => {
         const interval = setInterval(() => {
             setCurrentMessageIndex((currentMessageIndex + 1) % messages.length)
@@ -38,106 +36,91 @@ const Header = () => {
 
     const currentMessage = messages[currentMessageIndex]
 
-    function MenuClicked(){
-        setMenuOverlay(!MenuOverlay);
+    function MenuClicked() {
+        setMenuOverlay(!MenuOverlay)
     }
-    const [MenuOverlay, setMenuOverlay] = useState(false);
+    const [MenuOverlay, setMenuOverlay] = useState(false)
 
-    
     return (
-        <HeaderContainer >
+        <HeaderContainer>
+            <div className="TopText">
+                <a
+                    href="tel:+12155991400"
+                    style={{
+                        color: 'white',
+                        textDecoration: 'none',
+                        textAlign: 'center',
+                    }}
+                >
+                    {currentMessage}
+                </a>
+            </div>
 
-
-
-            <div className='TopText'>
-            <a
-              href="tel:+12155991400"
-              style={{ color: 'white', textDecoration: 'none',textAlign:"center" }}
-          >
-              {currentMessage}
-          </a>
-          </div>
-          
-          
-<div className='HolderTemp'>
-            <Grid container gap={0} className={navbar ? "Navbar" : "NavbarActive"}>
-                <Grid item lg={8.8}  sm={5} xs={6} className='LogoHolder'>
-                            <div className='LogoName'>
-                        Lessin Law
+            <div className="HolderTemp">
+                <Grid
+                    container
+                    gap={0}
+                    className={navbar ? 'Navbar' : 'NavbarActive'}
+                >
+                    <Grid item lg={8.8} sm={5} xs={6} className="LogoHolder">
+                        <div className="LogoName">Lessin Law</div>
+                    </Grid>
+                    <Grid item lg={3} sm={5} xs={4} className="IconHolderMain">
+                        <div className="DesktopView">
+                            <Grid container gap={2}>
+                                <Grid item lg={1.8} className="IconHolder">
+                                    <div className="MenuIcon">Home</div>
+                                </Grid>
+                                <Grid item lg={2}>
+                                    <div className="MenuIcon">Profiles</div>
+                                </Grid>
+                                <Grid item lg={2}>
+                                    <div className="MenuIcon">Reviews</div>
+                                </Grid>
+                                <Grid item lg={2}>
+                                    <div className="MenuIcon">Articles</div>
+                                </Grid>
+                                <Grid item lg={2}>
+                                    <div className="MenuIcon">Login</div>
+                                </Grid>
+                            </Grid>
                         </div>
+                        <MenuIcon
+                            className="MenuIconOver"
+                            onClick={MenuClicked}
+                        />
+                    </Grid>
                 </Grid>
-                <Grid item lg={3} sm={5} xs={4} className='IconHolderMain'>
-                    <div className='DesktopView'>
-                        <Grid container gap={2}>
-                            <Grid item lg={1.8} className='IconHolder'>
-                                <div className='MenuIcon'>
-                                    Home
-                                </div>
-                            </Grid>
-                            <Grid item lg={2}>
-                                <div className='MenuIcon'>
-                                    Profiles
-                                </div>
-                            </Grid>
-                            <Grid item lg={2}>
-                                <div className='MenuIcon'>
-                                    Reviews
-                                </div>
-                            </Grid>
-                            <Grid item lg={2}>
-                                <div className='MenuIcon'>
-                                    Articles
-                                </div>
-                            </Grid>
-                            <Grid item lg={2}>
-                                <div className='MenuIcon' >
-                                    Login
-                                </div>
-                            </Grid>
-                        </Grid>
+            </div>
+            {MenuOverlay && (
+                <motion.div
+                    className="NavButtonHolder"
+                    initial={{ y: -100 }} // Start from -100 on the y-axis
+                    animate={{ y: 0 }} // Move to 0 on the y-axis
+                    transition={{ type: 'spring', stiffness: 120, damping: 20 }}
+                >
+                    <div className="HolderSubDiv">
+                        <KeyboardArrowRightIcon className="SubHolderIcon" />
+                        <div className="SubHolderText">Home</div>
                     </div>
-                    <MenuIcon className='MenuIconOver' onClick={MenuClicked}/>
-                </Grid>
-
-                {/* <Grid item  xs={12} style={{height:,backgroundColor:"black"}}>
-
-                            
-                </Grid> */}
-
-
-            </Grid>
-
-            
-            </div>
-            {MenuOverlay &&  
-            <motion.div className='NavButtonHolder'
-            initial={{ y: -100 }}  // Start from -100 on the y-axis
-      animate={{ y: 0 }}     // Move to 0 on the y-axis
-      transition={{ type: "spring", stiffness: 120, damping: 20 }}>
-            <div className='HolderSubDiv'> 
-                <KeyboardArrowRightIcon className='SubHolderIcon'/>
-                <div className='SubHolderText'>Home</div>
-            </div>
-            <div className='HolderSubDiv'> 
-                <KeyboardArrowRightIcon className='SubHolderIcon'/>
-                <div className='SubHolderText'>Profiles</div>
-            </div>
-            <div className='HolderSubDiv'> 
-                <KeyboardArrowRightIcon className='SubHolderIcon'/>
-                <div className='SubHolderText'>Reviews</div>
-            </div>
-            <div className='HolderSubDiv'> 
-                <KeyboardArrowRightIcon className='SubHolderIcon'/>
-                <div className='SubHolderText'>Article</div>
-            </div>
-            <div className='HolderSubDiv'> 
-                <KeyboardArrowRightIcon className='SubHolderIcon'/>
-                <div className='SubHolderText'>Login</div>
-            </div>
-           
-    </motion.div>}
-            
-            
+                    <div className="HolderSubDiv">
+                        <KeyboardArrowRightIcon className="SubHolderIcon" />
+                        <div className="SubHolderText">Profiles</div>
+                    </div>
+                    <div className="HolderSubDiv">
+                        <KeyboardArrowRightIcon className="SubHolderIcon" />
+                        <div className="SubHolderText">Reviews</div>
+                    </div>
+                    <div className="HolderSubDiv">
+                        <KeyboardArrowRightIcon className="SubHolderIcon" />
+                        <div className="SubHolderText">Article</div>
+                    </div>
+                    <div className="HolderSubDiv">
+                        <KeyboardArrowRightIcon className="SubHolderIcon" />
+                        <div className="SubHolderText">Login</div>
+                    </div>
+                </motion.div>
+            )}
         </HeaderContainer>
     )
 }
@@ -146,138 +129,133 @@ const HeaderContainer = styled.header`
     top: 0;
     left: 0;
     right: 0;
-    height:100px;
+    height: 100px;
     color: #fff;
-    position:fixed;
-    z-index:100;
-    display:flex;
-    flex-direction:column;
-    align-content:center;
-    align-items:center;
-    .MenuIconOver{
-        display:none;
+    position: fixed;
+    z-index: 100;
+    display: flex;
+    flex-direction: column;
+    align-content: center;
+    align-items: center;
+    .MenuIconOver {
+        display: none;
     }
-    .TopText{
-        background-color:black;
-        width:100%;
-        display:flex;
-        flex-direction:column;
-        height:50px;
-        justify-content:center;
-        align-content:center;
-        align-items:center;
+    .TopText {
+        background-color: black;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        height: 50px;
+        justify-content: center;
+        align-content: center;
+        align-items: center;
     }
-    .Navbar{
-        background-color:rgba(1, 32, 48, 0.5);
-        height:100%;
-        display:none;
+    .Navbar {
+        background-color: rgba(1, 32, 48, 0.5);
+        height: 100%;
+        display: none;
     }
-    .NavbarActive{
-        background-color:rgba(1, 32, 48, 1);
-        height:100%;
+    .NavbarActive {
+        background-color: rgba(1, 32, 48, 1);
+        height: 100%;
     }
 
-    .LogoName{
-        font-size:35px;
-        color:white;
-        font-weight:700;
-        padding-left:2.5%;
+    .LogoName {
+        font-size: 35px;
+        color: white;
+        font-weight: 700;
+        padding-left: 2.5%;
     }
-    .LogoHolder{
-        display:flex;
-        height:100%;
-        align-content:center;
-        align-items:center;
-        font-weight:100;
+    .LogoHolder {
+        display: flex;
+        height: 100%;
+        align-content: center;
+        align-items: center;
+        font-weight: 100;
     }
-    .IconHolderMain{
-        display:grid;
-        align-items:center;
-        align-content:center;
-        width:100%;
+    .IconHolderMain {
+        display: grid;
+        align-items: center;
+        align-content: center;
+        width: 100%;
     }
-    .MenuIcon{
-        font-size:18px;
-        font-weight:100;
+    .MenuIcon {
+        font-size: 18px;
+        font-weight: 100;
     }
-    .MenuIcon:hover{
-        cursor:pointer;
+    .MenuIcon:hover {
+        cursor: pointer;
     }
-    .HolderTemp{
-        width:100%;
+    .HolderTemp {
+        width: 100%;
     }
-    .NavButtonHolder{
-        display:none;
+    .NavButtonHolder {
+        display: none;
     }
 
     @media (max-width: 768px) {
-        
-        .NavButtonHolder{
-            display:unset;
-            background-color:rgba(1, 32, 48, 1);
-            width:100%;
+        .NavButtonHolder {
+            display: unset;
+            background-color: rgba(1, 32, 48, 1);
+            width: 100%;
         }
-        .HolderSubDiv{
-            display:flex;
-            align-items:center;
-            padding:3%;
+        .HolderSubDiv {
+            display: flex;
+            align-items: center;
+            padding: 3%;
         }
-        .SubHolderText{
-            color:white;
-            font-size:20px;
-            font-weight:100;
-            padding-left:5%;
+        .SubHolderText {
+            color: white;
+            font-size: 20px;
+            font-weight: 100;
+            padding-left: 5%;
         }
-        .SubHolderIcon{
-            color:white;
-            font-size:25px;
+        .SubHolderIcon {
+            color: white;
+            font-size: 25px;
         }
-        
-        .DesktopView{
-            display:none;
+
+        .DesktopView {
+            display: none;
         }
-        .LogoName{
-            padding-left:5%;
+        .LogoName {
+            padding-left: 5%;
         }
-        .MenuIcon{
-            right:5%;
-            font-size:35px;
-            position:absolute;
+        .MenuIcon {
+            right: 5%;
+            font-size: 35px;
+            position: absolute;
         }
-        .MenuIconOver{
-            display:unset;
-            right:5%;
-            font-size:35px;
-            position:absolute;
+        .MenuIconOver {
+            display: unset;
+            right: 5%;
+            font-size: 35px;
+            position: absolute;
         }
-       
-            .MenuOverlay {
-                position: fixed;
-                top: 80px;
-                right: 15px;
-                width: 200px;
-                border-radius:5px;
-                height: auto;
-                background-color: rgba(1, 32, 48, 0.95);
-                padding: 20px;
-                box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-                z-index: 1000;
-              }
-              
-              .menu-option {
-                margin-bottom: 15px;
-                color: white;
-                font-weight:400;
-                font-size: 20px;
-                cursor: pointer;
-              }
-       
-        
+
+        .MenuOverlay {
+            position: fixed;
+            top: 80px;
+            right: 15px;
+            width: 200px;
+            border-radius: 5px;
+            height: auto;
+            background-color: rgba(1, 32, 48, 0.95);
+            padding: 20px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+            z-index: 1000;
+        }
+
+        .menu-option {
+            margin-bottom: 15px;
+            color: white;
+            font-weight: 400;
+            font-size: 20px;
+            cursor: pointer;
+        }
     }
-    
+
     @media (min-width: 800px) and (max-width: 950px) {
-        
     }
-    
 `
 export default Header
