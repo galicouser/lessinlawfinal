39b09a4416c14474cb108895a1088ff8df30b16f
pauldidonatojr <pjdidonatojr@gmail.com>
2023-10-24 07:33:16 -0400

    push me

--- src/components/BoutiqueLitigationFirm.js
+++ src/components/BoutiqueLitigationFirm.js
@@ -1,58 +1,59 @@
 import React from 'react'
 import { BoutiqueLitigation } from '../styled-components/HomePageStyles'
-import { useNavigate } from 'react-router-dom';
-import ChevronRightRoundedIcon from '@mui/icons-material/ChevronRightRounded';
+import { useNavigate } from 'react-router-dom'
+import ChevronRightRoundedIcon from '@mui/icons-material/ChevronRightRounded'
 const BoutiqueLitigationFirm = () => {
-    const navigate = useNavigate();
-    return (
-     <BoutiqueLitigation>
-      <div className="LitigationText">
-       <p className="MainText">BOUTIQUE LITIGATION FIRM</p>
-       <p className="ParaText">
-        Jeffrey R. Lessin & Associates PC is a distinguished litigation firm
-        that prioritizes delivering top-notch legal guidance to clients on a
-        national and international scale. Operating from our offices in
-        Philadelphia, Pennsylvania, and Marlton, New Jersey, we serve a broad
-        spectrum of clients, including major Fortune 100 corporations and
-        individuals, across various civil and commercial cases.
-       </p>
-       <p className="ParaText">
-        Established more than twenty years ago, Lessin Law has a steadfast
-        commitment to achieving excellence. We possess the expertise,
-        experience, and assets required to navigate the most intricate legal
-        challenges and secure triumphs, especially when the circumstances are
-        most demanding. Our firm provides a dynamic practice with significant
-        legal influence and a well-demonstrated history of accomplishments.
-       </p>
-       <div
-        className="ReadAction"
-        onClick={() => {
-         navigate('/About')
-        }}
-       >
-        <div className="LayeredColor"></div>
-        <div className="LayeredColor2"></div>
+ const navigate = useNavigate()
+ return (
+  <BoutiqueLitigation>
+   <div className="LitigationText">
+    <p className="MainText">BOUTIQUE LITIGATION FIRM</p>
+    <p className="ParaText">
+     Jeffrey R. Lessin & Associates PC, located in Philadelphia, Pennsylvania,
+     is a premier personal injury litigation firm. We specialize in providing
+     outstanding legal representation for personal injury cases, serving both
+     individuals and major Fortune 100 corporations. If you're searching for a
+     dedicated and experienced personal injury lawyer in Philadelphia, look no
+     further than our trusted team.
+    </p>
+    <p className="ParaText">
+     Established over two decades ago, Jeffrey R. Lessin & Associates PC has an
+     unwavering commitment to legal excellence, especially in the realm of
+     personal injury. With vast expertise and experience, our team has the
+     essential assets to tackle complex legal challenges and deliver victories,
+     particularly in demanding situations. If you're in search of a personal
+     injury lawyer with a proven track record and significant legal influence in
+     Philadelphia, our firm stands as a testament to consistent accomplishment.
+    </p>
+    <div
+     className="ReadAction"
+     onClick={() => {
+      navigate('/About')
+     }}
+    >
+     <div className="LayeredColor"></div>
+     <div className="LayeredColor2"></div>
 
-        <div className="LayeredColor3"></div>
-        <div className="ActionText">LEARN MORE</div>
-        <ChevronRightRoundedIcon className="ActionIcon" />
-       </div>
-      </div>
-      <div className="ImagesHolder">
-       <img
-        src="https://www.tlgattorneys.com/wp-content/themes/paperstreet/images/c1.jpg"
-        className="Image"
-       />
-       <img
-        src="https://www.tlgattorneys.com/wp-content/themes/paperstreet/images/c2.jpg"
-        className="Image"
-       />
-       <img
-        src="https://www.tlgattorneys.com/wp-content/themes/paperstreet/images/c3.jpg"
-        className="Image"
-       />
-      </div>
-     </BoutiqueLitigation>
-    )
+     <div className="LayeredColor3"></div>
+     <div className="ActionText">LEARN MORE</div>
+     <ChevronRightRoundedIcon className="ActionIcon" />
+    </div>
+   </div>
+   <div className="ImagesHolder">
+    <img
+     src="https://www.tlgattorneys.com/wp-content/themes/paperstreet/images/c1.jpg"
+     className="Image"
+    />
+    <img
+     src="https://www.tlgattorneys.com/wp-content/themes/paperstreet/images/c2.jpg"
+     className="Image"
+    />
+    <img
+     src="https://www.tlgattorneys.com/wp-content/themes/paperstreet/images/c3.jpg"
+     className="Image"
+    />
+   </div>
+  </BoutiqueLitigation>
+ )
 }
 export default BoutiqueLitigationFirm
