28150790eff7ce7cddc65a39ea5a9274cba04cff
Fahad Bashir <fahadbashir757@gmail.com>
2024-10-27 05:13:15 +0500

    added env api url

--- server/index.js
+++
@@ -1,7 +0,0 @@
-const app = require('./server.js'); // Use require instead of import
-
-const PORT = process.env.NODE_PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`App is listening on port: ${PORT}`);
-});
--- server/routes/contactRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { contactController } = require('../controllers/contactController.js');
-
-const router = express.Router();
-
-// Define the route for contact
-router.post('/', contactController);
-
-module.exports = router; // Export the router using CommonJS
